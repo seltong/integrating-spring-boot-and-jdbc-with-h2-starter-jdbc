da472fe2441daf6281142efba445a1dcf490d8be
Selton Guedes <sfgs@ecomp.poli.br>
2022-01-20 10:51:22 -0300

    Initial commit

---
+++ spring-boot-2-jdbc-with-h2/src/test/java/com/in28minutes/springboot/rest/example/springboot2jdbcwithh2/SpringBoot2JdbcWithH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.rest.example.springboot2jdbcwithh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2JdbcWithH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
